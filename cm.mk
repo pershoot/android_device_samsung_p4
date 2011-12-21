4f9a34c8fb39a77b9678bc3b02bc7d5bd3e65f5a
Prashant Somashekar <prashsomash@gmail.com>
2011-12-21 14:55:28 -0800

    p4: Initial import of GT-P7500 device
    -referenced p4tmo

---
+++ cm.mk
@@ -0,0 +1,21 @@
+# Inherit device configuration for p3.
+$(call inherit-product, device/samsung/p3/p3.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inhert GSM common stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_p4
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := p4
+PRODUCT_MODEL := GT-P7500
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P7500 BUILD_ID=HTJ85B BUILD_FINGERPRINT=samsung/GT-P7500/GT-P7500:3.2/HTJ85B/XWKK4:user/release-keys PRIVATE_BUILD_DESC="GT-P7500-user 3.2 HTJ85B XWKK4 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := p4
---
+++ p4.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/modules/dhd.ko:system/lib/modules/dhd.ko
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product, device/samsung/p4-common/p4-common.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/p4/p4-vendor.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := p4
+PRODUCT_DEVICE := p4
+PRODUCT_MODEL := p4
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
