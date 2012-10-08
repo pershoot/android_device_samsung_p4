bde148525da677c7545b13d93a231b834b47c191
Prashant Somashekar <prashsomash@gmail.com>
2012-10-08 13:33:00 -0400

    p4: add in 3.1 kernel and associated changes
    -reference isimobile (ril) and remove HC_RADIO legacy
    -change wlan interface
    -amend props
    -change tty interface
    -enable voice_capable
     (for more info: http://forum.xda-developers.com/showthread.php?t=1875024)
    -remove libarccamera, libseccamera_jni and libseccameracore
    -add my.gps
    -add gps.xml (from stock)
    -add apns (from stock)
    
    Change-Id: Id3f8e6e2bf64fcc43e52e749e1323c4c8802dcfd

--- p4.mk
+++ p4.mk
@@ -22,9 +22,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/modules/bcmdhd.ko:system/lib/modules/bcmdhd.ko \
+    $(LOCAL_PATH)/modules/dhd.ko:system/lib/modules/dhd.ko \
     $(LOCAL_PATH)/modules/cifs.ko:system/lib/modules/cifs.ko \
     $(LOCAL_PATH)/modules/ntfs.ko:system/lib/modules/ntfs.ko
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/cbd:root/sbin/cbd \
+    $(LOCAL_PATH)/gps.xml:system/etc/gps.xml \
+    $(LOCAL_PATH)/apns/apns-conf.xml:system/etc/apns-conf.xml
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
@@ -40,6 +45,14 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
   Mms
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.telephony.ril_class=Smdk4210RIL
+
+# GPS config
+PRODUCT_PROPERTY_OVERRIDES += \
+  my.gps=novzw
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := p4
